f13e053824bda4e803ee5706c914fafe3484e66f
Mohamed Gouda <muhammed.gouda93@gmail.com>
2022-04-16 02:04:33 +0200

    image-streaming-first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'y';
+
+  title = 'image-streaming';
+
+  baseURL:string= 'http://localhost:8090/images/'
+  currentPhoto: string =""
+
+  constructor(private socket: Socket) {
+    this.connectToSocketAndChange()
+    this.currentPhoto = "8.png"
+  }
+
+
+  changePhoto(data:string){
+    this.currentPhoto = data
+  }
+
+  connectToSocketAndChange = () => {
+    this.socket.on("file_changed", (data:any) => {
+      this.changePhoto(data);
+    });
+
+
+    this.socket.emit("currentFile", (cb:any) => {
+      this.changePhoto(cb);
+    });
+   
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const config: SocketIoConfig = { url: 'ws://127.0.0.1:8090', options: {
+    reconnection: true,
+    transports: ["websocket", "polling"],
+    }
+};
+
 
 @NgModule({
   declarations: [
@@ -10,9 +19,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
